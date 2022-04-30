2073c2ef15b6d7a3ff90f25ff8f23400ae0887f2
wilson-castro <wilsoncastro.pv@gmail.com>
2022-04-30 09:54:55 -0300

    Primeiro commit, início do projeto

---
+++ ignews/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | Ig.News</title>
+      </Head>
+
+      <h1>
+        Helo <span>World</span>
+      </h1>
+    </>
+  )
+}
